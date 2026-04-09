7abb665ec7c2b2609909165f7b9b719ed68c3e1e
Joshua Luppes <lupjos@dunwoody.edu>
2026-04-09 10:56:27 -0500

    Add logging

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template, current_app
 from . import main_blueprint
 
 
 @main_blueprint.route('/')
 def home():
+    current_app.logger.info("Index page loading")
     return render_template('main/index.html')
